11a780c2eb8c6de14a7f332622b186cf9ce5cad5
srinijammula <srinijammula5@gmail.com>
2025-07-01 17:51:46 -0400

    usestate filter for top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,29 @@
 import { RestaurantCard } from "./RestaurantCard"
 import restaurants from "../utils/data"
+import {useState} from "react";
 
 export const Body = () => {
-    const restaurantList = [];
+    const [restaurantList, setRestaurantList] = useState(restaurants);
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
-                    console.log("yes it works")
+                    // console.log("yes it works")
+                    const newRestaurantList = restaurantList.filter(
+                        (res) => res.info.avgRating>4
+                    )
+                    setRestaurantList(newRestaurantList)
                 }}
                 >Top Rated Restaurants</button>
+                <button className="filter-all-btn" onClick={() => {
+                    setRestaurantList(restaurants)
+                }}
+                >Show all Restaurants</button>
+                
             </div>
             <div className="res-container">
                 {
-                    restaurants.map(restaurant=> <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+                    restaurantList.map(restaurant=> <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
                 }
             </div>
         </div>
