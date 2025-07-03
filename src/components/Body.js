e646bd935dfa0bb5abdcb0cd6c21cf34661f1bbe
srinijammula <srinijammula5@gmail.com>
2025-07-02 20:55:41 -0400

    handle empty list in search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,19 @@
 import { RestaurantCard } from "./RestaurantCard"
-import restaurants from '../utils/data'
+// import restaurants from '../utils/data'
 import {useState, useEffect} from "react";
 import { corsProxy, swiggyApi } from "../utils/constants"
 import {Shimmer} from "./shimmer"
 
 export const Body = () => {
     const [restaurantList, setRestaurantList] = useState([]);
+    const [filteredList, setFilteredList] = useState([]);
     const [searchText, setSearchText] =useState("");
 
     const fetchData= async () => {
         const data = await fetch(corsProxy+swiggyApi)
         const json=await data.json();
         setRestaurantList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setFilteredList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
      useEffect(() => {
@@ -30,26 +32,31 @@ export const Body = () => {
                     <input type="text" className="search-box" value={searchText} onChange={(e) => {setSearchText(e.target.value) }}/>
                     <button className="search-btn" onClick={() => {
                         const filteredList = restaurantList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
-                        console.log(filteredList)
-                        setRestaurantList(filteredList)}} >
+                        if(filteredList.length==0){
+                            setFilteredList(restaurantList)
+                            //setRestaurantList(newList);
+                        }
+                        else{
+                        setFilteredList(filteredList)}}
+                     } >
                         Search</button>
                 </div>
                 <button className="filter-btn" onClick={() => {
                     const newRestaurantList = restaurantList.filter(
                         (res) => res.info.avgRating>4.3
                     )
-                    setRestaurantList(newRestaurantList)
+                    setFilteredList(newRestaurantList)
                 }}
                 >Top Rated Restaurants</button>
                 <button className="filter-all-btn" onClick={() => {
-                    setRestaurantList(restaurants)
+                    setFilteredList(restaurantList)
                 }}
                 >Show all Restaurants</button>
                 
             </div>
             <div className="res-container">
                 {
-                    restaurantList.map(restaurant=> <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+                    filteredList.map(restaurant=> <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
                 }
             </div>
         </div>
